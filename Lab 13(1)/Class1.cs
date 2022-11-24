c3091aa5c508aa538bcd69452e75c25baf7aa11d
krist <krist@LAPTOP-HIQLHLL6>
2022-11-25 00:18:18 +0500

    Добавьте файлы проекта.

---
+++ Lab 13(1)/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_13
+{
+    class Building
+    {
+        string adres;
+        int shirina;
+        int dlina;
+        int visota;
+
+        int Shirina
+        {
+            get
+            {
+                return shirina;
+            }
+            set
+            {
+                if (value < 0)
+                    shirina = 10;
+                else
+                    shirina = value;
+            }
+        }
+
+        public Building(string adres, int shirina, int dlina, int visota)
+        {
+            this.adres = adres;
+            this.shirina = shirina;
+            this.dlina = dlina;
+            this.visota = visota;
+        }
+        public string Print()
+        {
+            return $"{adres}, {shirina}, {dlina}";
+        }
+    }
+}
+}
